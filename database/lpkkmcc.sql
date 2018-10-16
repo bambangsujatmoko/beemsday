8ec8d7c6339aeabc669dba225d4923555447e087
bambangsujatmoko <bambangdon@gmail.com>
2018-10-16 14:24:48 +0700

    Add files via upload

---
+++ database/lpkkmcc.sql
@@ -0,0 +1,19 @@
+/*
+SQLyog Enterprise - MySQL GUI v7.14 
+MySQL - 5.6.26 : Database - lpkkmcc
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`lpkkmcc` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `lpkkmcc`;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
